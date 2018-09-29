cb4d099f1d84e3bdbf6fbb4fa30d9beaaf6368e5
pablofr <pablo.freitas28@gmail.com>
2018-09-28 20:30:52 -0400

    'Inicio'

---
+++ src/main/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>Candidatos Eleições 2018</h1>
+    </div>
+)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+}
